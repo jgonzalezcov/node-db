cfd1a1c1e51c5d69160ce8403f6e9b425adb2c8a
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-12-30 23:19:20 -0300

    feat:Server file is created and with the post add

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(express.static('public'))
+app.use(cors())
+app.use(express.json())
+const CsbInspector = require('csb-inspector')
+CsbInspector()
+require('dotenv').config()
+const { obtenerPosts, agregarPosts } = require('./api/models/postModels')
+const {
+  validateInput,
+  validateURL,
+  validaformat,
+} = require('./helpers/validate')
+const errorServer = {
+  status: 500,
+  statusText: 'error',
+  text: 'Error interno del servidor',
+}
+
+/**********************************Levanta el Servidor***************************************/
+app.listen(process.env.PORT, () => {
+  console.log('El servidor esta activo en el puerto', process.env.PORT)
+})
+/**************************Envia archivo a mostrar en el Home*******************************/
+app.get('/', (req, res) => {
+  try {
+    return res.sendFile(__dirname + '/public/index.html')
+  } catch (error) {
+    res
+      .status(errorServer.status)
+      .send({ status: errorServer.statusText, data: errorServer.text })
+  }
+})
+
+/****************************Endpoint para crear Posts**********************************/
+app.post('/posts', async (req, res) => {
+  try {
+    const payload = req.body
+    resp = await validateInput(payload)
+    if (resp.status === 200) {
+      await agregarPosts(payload)
+      res.send('Post creado con exito')
+    }
+  } catch (error) {
+    res
+      .status(errorServer.status)
+      .send({ status: errorServer.statusText, data: errorServer.text })
+  }
+})
