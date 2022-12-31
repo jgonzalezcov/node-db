fbb60f05d54a251945d27d5ecf4cb57dc8f38591
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-12-30 23:07:52 -0300

    feat:Create functions that query the database

---
+++ api/models/postModels.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg')
+const credentials = require('../../config/postgressql')
+const pool = new Pool(credentials)
+
+/*******************Cominucación a bases de datos Postgressql para que trae todos los posts**********************/
+const obtenerPosts = async () => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM posts')
+    return rows
+  } catch (e) {
+    console.log('error al insertar datos en tabla product: ', e.code, e.message)
+    throw new Error(e)
+  }
+}
+
+const agregarPosts = async (payload) => {
+  const SQLquery = {
+    text: 'INSERT INTO posts (id, title, img, description, likes) VALUES (DEFAULT, $1, $2, $3, $4) RETURNING *',
+    values: [payload.titulo, payload.url, payload.descripcion, payload.likes],
+  }
+  try {
+    const result = await pool.query(SQLquery)
+    return result.rows
+  } catch (e) {
+    console.log('error al insertar datos en tabla product: ', e.code, e.message)
+    throw new Error(e)
+  }
+}
+
+module.exports = { obtenerPosts, agregarPosts }
